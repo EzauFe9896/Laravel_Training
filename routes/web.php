2c7404fac346f123da7ad9d59c566d3f8833cb27
EzauFe9896 <joseuaze@gmail.com>
2020-08-13 23:45:04 -0600

    Subo la estructura del proyecto al repositorio de GitHub

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,8 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/about', function () {
+/*Route::get('/about', function () {
     return "This is about page";
 });
 
@@ -35,4 +35,13 @@ Route::get('/admin/post/example', array('as'=>'admin.home',function()
     $url= route('admin.home');
 
 return "This url is: " . $url;
-}));
+})); */
+
+
+//Route::get('/post/{id}', 'PostController@index');
+
+Route::resource('post', 'PostController');
+
+Route::get('/contact', 'PostController@contact');
+
+Route::get('post/{id}/{name}/{password}', 'PostController@show_post');
