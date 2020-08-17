7e710ced7b5d5c5f06dbce87875fecc258537d72
Ezau José <69482703+EzauFe9896@users.noreply.github.com>
2020-08-16 23:35:53 -0600

    Añado la vista contact
    En el vídeo # 26 se explica como crear una vista y como acceder a ella.

---
+++ contact.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>/</title>
+
+    <link href="https://fonts.googlerapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="container">
+
+    <h1>Contact Page People</h1>
+
+</div>
+</body>
+</html>
