fa3f7834430f740c2f1384e54d03eaababfe6035
Ezau José <69482703+EzauFe9896@users.noreply.github.com>
2020-08-19 11:02:08 -0600

    ModelCountry
    Se crea una función post para llamar el id country y el id user.

---
+++ Country.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    //
+public function post(){
+
+
+
+return $this->hasManyThrough('App\Post','App\User');
+
+}
+}
